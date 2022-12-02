56217cc8a1ea4765a89df542925edf21bdf9ebf7
unknown <dhanielbatista@gmail.com>
2022-12-01 23:07:43 -0300

    update profiles

---
+++ GashBellApi/Profiles/DemonProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GashBellApi.Data.Dtos.Demon;
+using GashBellApi.Models;
+
+namespace GashBellApi.Profiles
+{
+    public class DemonProfile : Profile
+    {
+        public DemonProfile()
+        {
+            CreateMap<CreateDemonDto, Demon>();
+            CreateMap<Demon, ReadDemonDto>();
+            CreateMap<Demon, UpdateDemonDto>();
+        }
+
+    }
+}
---
+++ GashBellApi/Profiles/HumanProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GashBellApi.Data.Dtos.Human;
+using GashBellApi.Models;
+
+namespace GashBellApi.Profiles
+{
+    public class HumanProfile : Profile
+    {
+        public HumanProfile()
+        {
+            CreateMap<CreateHumanDto, Human>();
+            CreateMap<Human, ReadHumanDto>();
+            CreateMap<Human, UpdateHumanDto>();
+        }
+
+    }
+}
